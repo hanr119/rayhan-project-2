3d404dfa69cce5ab79c363d80cf44e5d11e48357
hanr119 <70984148+hanr119@users.noreply.github.com>
2021-03-02 02:13:37 -0800

    final project

---
+++ glow.js
@@ -0,0 +1,86 @@
+let shadow1 = document.querySelector('.click-shadow1')
+let popup1 = document.querySelector('.popup-1')
+
+shadow1.addEventListener('click', () => {
+
+    popup1.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup1.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup1.classList.toggle('active');
+})
+
+
+let shadow2 = document.querySelector('.click-shadow2')
+let popup2 = document.querySelector('.popup-2')
+
+shadow2.addEventListener('click', () => {
+
+    popup2.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup2.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup2.classList.toggle('active');
+})
+
+
+let shadow3 = document.querySelector('.click-shadow3')
+let popup3 = document.querySelector('.popup-3')
+
+shadow3.addEventListener('click', () => {
+
+    popup3.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup3.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup3.classList.toggle('active');
+})
+
+
+let shadow4 = document.querySelector('.click-shadow4')
+let popup4 = document.querySelector('.popup-4')
+
+shadow4.addEventListener('click', () => {
+
+    popup4.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup4.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup4.classList.toggle('active');
+})
+
+
+let shadow5 = document.querySelector('.click-shadow5')
+let popup5 = document.querySelector('.popup-5')
+
+shadow5.addEventListener('click', () => {
+
+    popup5.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup5.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup5.classList.toggle('active');
+})
+
+
+let shadow6 = document.querySelector('.click-shadow6')
+let popup6 = document.querySelector('.popup-6')
+
+shadow6.addEventListener('click', () => {
+
+    popup6.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup6.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup6.classList.toggle('active');
+})
+
+
+let shadow7 = document.querySelector('.click-shadow7')
+let popup7 = document.querySelector('.popup-7')
+
+shadow7.addEventListener('click', () => {
+
+    popup7.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup7.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup7.classList.toggle('active');
+})
+
+
+let shadow8 = document.querySelector('.click-shadow8')
+let popup8 = document.querySelector('.popup-8')
+
+shadow8.addEventListener('click', () => {
+
+    popup8.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup8.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup8.classList.toggle('active');
+})
---
+++ light.js
@@ -0,0 +1,296 @@
+
+let shadow1 = document.querySelector('.click-shadow1')
+let popup1 = document.querySelector('.popup-1')
+
+shadow1.addEventListener('click', () => {
+
+    popup1.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup1.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup1.classList.toggle('active');
+})
+
+
+let shadow2 = document.querySelector('.click-shadow2')
+let popup2 = document.querySelector('.popup-2')
+
+shadow2.addEventListener('click', () => {
+
+    popup2.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup2.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup2.classList.toggle('active');
+})
+
+
+let shadow3 = document.querySelector('.click-shadow3')
+let popup3 = document.querySelector('.popup-3')
+
+shadow3.addEventListener('click', () => {
+
+    popup3.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup3.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup3.classList.toggle('active');
+})
+
+
+let shadow4 = document.querySelector('.click-shadow4')
+let popup4 = document.querySelector('.popup-4')
+
+shadow4.addEventListener('click', () => {
+
+    popup4.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup4.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup4.classList.toggle('active');
+})
+
+
+let shadow5 = document.querySelector('.click-shadow5')
+let popup5 = document.querySelector('.popup-5')
+
+shadow5.addEventListener('click', () => {
+
+    popup5.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup5.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup5.classList.toggle('active');
+})
+
+
+let shadow6 = document.querySelector('.click-shadow6')
+let popup6 = document.querySelector('.popup-6')
+
+shadow6.addEventListener('click', () => {
+
+    popup6.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup6.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup6.classList.toggle('active');
+})
+
+
+let shadow7 = document.querySelector('.click-shadow7')
+let popup7 = document.querySelector('.popup-7')
+
+shadow7.addEventListener('click', () => {
+
+    popup7.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup7.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup7.classList.toggle('active');
+})
+
+
+let shadow8 = document.querySelector('.click-shadow8')
+let popup8 = document.querySelector('.popup-8')
+
+shadow8.addEventListener('click', () => {
+
+    popup8.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup8.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup8.classList.toggle('active');
+})
+
+
+let shadow9 = document.querySelector('.click-shadow9')
+let popup9 = document.querySelector('.popup-9')
+
+shadow9.addEventListener('click', () => {
+
+    popup9.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup9.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup9.classList.toggle('active');
+})
+
+
+let shadow10 = document.querySelector('.click-shadow10')
+let popup10 = document.querySelector('.popup-10')
+
+shadow10.addEventListener('click', () => {
+
+    popup10.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup10.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup10.classList.toggle('active');
+})
+
+
+let shadow11 = document.querySelector('.click-shadow11')
+let popup11 = document.querySelector('.popup-11')
+
+shadow11.addEventListener('click', () => {
+
+    popup11.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup11.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup11.classList.toggle('active');
+})
+
+
+let shadow12 = document.querySelector('.click-shadow12')
+let popup12 = document.querySelector('.popup-12')
+
+shadow12.addEventListener('click', () => {
+
+    popup12.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup12.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup12.classList.toggle('active');
+})
+
+
+let shadow13 = document.querySelector('.click-shadow13')
+let popup13 = document.querySelector('.popup-13')
+
+shadow13.addEventListener('click', () => {
+
+    popup13.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup13.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup13.classList.toggle('active');
+})
+
+
+let shadow14 = document.querySelector('.click-shadow14')
+let popup14 = document.querySelector('.popup-14')
+
+shadow14.addEventListener('click', () => {
+
+    popup14.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup14.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup14.classList.toggle('active');
+})
+
+
+let shadow15 = document.querySelector('.click-shadow15')
+let popup15 = document.querySelector('.popup-15')
+
+shadow15.addEventListener('click', () => {
+
+    popup15.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup15.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup15.classList.toggle('active');
+})
+
+
+let shadow16 = document.querySelector('.click-shadow16')
+let popup16 = document.querySelector('.popup-16')
+
+shadow16.addEventListener('click', () => {
+
+    popup16.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup16.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup16.classList.toggle('active');
+})
+
+
+let shadow17 = document.querySelector('.click-shadow17')
+let popup17 = document.querySelector('.popup-17')
+
+shadow17.addEventListener('click', () => {
+
+    popup17.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup17.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup17.classList.toggle('active');
+})
+
+
+let shadow18 = document.querySelector('.click-shadow18')
+let popup18 = document.querySelector('.popup-18')
+
+shadow18.addEventListener('click', () => {
+
+    popup18.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup18.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup18.classList.toggle('active');
+})
+
+
+let shadow19 = document.querySelector('.click-shadow19')
+let popup19 = document.querySelector('.popup-19')
+
+shadow19.addEventListener('click', () => {
+
+    popup19.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup19.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup19.classList.toggle('active');
+})
+
+
+let shadow20 = document.querySelector('.click-shadow20')
+let popup20 = document.querySelector('.popup-20')
+
+shadow20.addEventListener('click', () => {
+
+    popup20.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup20.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup20.classList.toggle('active');
+})
+
+
+let shadow21 = document.querySelector('.click-shadow21')
+let popup21 = document.querySelector('.popup-21')
+
+shadow21.addEventListener('click', () => {
+
+    popup21.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup21.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup21.classList.toggle('active');
+})
+
+
+let shadow22 = document.querySelector('.click-shadow22')
+let popup22 = document.querySelector('.popup-22')
+
+shadow22.addEventListener('click', () => {
+
+    popup22.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup22.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup22.classList.toggle('active');
+})
+
+
+let shadow23 = document.querySelector('.click-shadow23')
+let popup23 = document.querySelector('.popup-23')
+
+shadow23.addEventListener('click', () => {
+
+    popup23.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup23.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup23.classList.toggle('active');
+})
+
+
+let shadow24 = document.querySelector('.click-shadow24')
+let popup24 = document.querySelector('.popup-24')
+
+shadow24.addEventListener('click', () => {
+
+    popup24.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup24.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup24.classList.toggle('active');
+})
+
+
+let shadow25 = document.querySelector('.click-shadow25')
+let popup25 = document.querySelector('.popup-25')
+
+shadow25.addEventListener('click', () => {
+
+    popup25.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup25.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup25.classList.toggle('active');
+})
+
+
+let shadow26 = document.querySelector('.click-shadow26')
+let popup26 = document.querySelector('.popup-26')
+
+shadow26.addEventListener('click', () => {
+
+    popup26.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup26.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup26.classList.toggle('active');
+})
+
+
+let shadow27 = document.querySelector('.click-shadow27')
+let popup27 = document.querySelector('.popup-27')
+
+shadow27.addEventListener('click', () => {
+
+    popup27.style.top = document.body.clientHeight * Math.random() + 'px';
+    popup27.style.left = document.body.clientWidth * Math.random() + 'px';
+    popup27.classList.toggle('active');
+})
